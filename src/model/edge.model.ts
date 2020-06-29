e4606de69352c92d76ef14c9eb5bb80d1aecfe9b
Gábor Marczi <marczi.gabor@outlook.com>
2020-06-29 20:48:24 +0200

    tangents

--- src/model/edge.model.ts
+++ src/model/edge.model.ts
@@ -38,12 +38,42 @@ export class Edge implements IRendering {
 
         context.strokeStyle = 'blue';
 
-        context.beginPath();
-        context.moveTo(this.startNode.position.x, this.startNode.position.y);
-        context.lineTo(this.endNode.position.x, this.endNode.position.y);
+        // context.beginPath();
+        // context.moveTo(this.startNode.position.x, this.startNode.position.y);
+        // context.lineTo(this.endNode.position.x, this.endNode.position.y);
+        // context.lineWidth = 1;
+        // context.stroke();                    
+        // context.closePath();
+
+        var dx = this.endNode.position.x - this.startNode.position.x;
+        var dy = this.endNode.position.y - this.startNode.position.y;
+        var dist = Math.sqrt(dx*dx + dy*dy);
+    
+        // if (dist <= Math.abs(r2 - r1)) {
+        //     // The circles are coinciding. There are no valid tangents.
+        //     return;
+        // }
+    
+        // Rotation from the x-axis.
+        var angle1 = Math.atan2(dy, dx);
+    
+        // Relative angle of the normals. This is equal for both circles.
+        var angle2 = Math.acos((this.startNode.radius - this.endNode.radius)/dist);
+
+        context.strokeStyle = 'blue';
         context.lineWidth = 1;
+        
+        context.beginPath();
+        context.moveTo(this.startNode.position.x + this.startNode.radius * Math.cos(angle1 + angle2), this.startNode.position.y + this.startNode.radius * Math.sin(angle1 + angle2));
+        context.lineTo(this.endNode.position.x + this.endNode.radius * Math.cos(angle1 + angle2), this.endNode.position.y + this.endNode.radius * Math.sin(angle1 + angle2));
         context.stroke();                    
         context.closePath();
+
+        context.beginPath();
+        context.moveTo(this.startNode.position.x + this.startNode.radius * Math.cos(angle1 - angle2), this.startNode.position.y + this.startNode.radius * Math.sin(angle1 - angle2));
+        context.lineTo(this.endNode.position.x + this.endNode.radius * Math.cos(angle1 - angle2), this.endNode.position.y + this.endNode.radius * Math.sin(angle1 - angle2));
+        context.stroke();                    
+        context.closePath();        
 
     }
 }
--- src/model/node.model.ts
+++ src/model/node.model.ts
@@ -60,5 +60,50 @@ export class Node implements IRendering {
         context.arc(this._position.x, this._position.y, this._radius, 0, 2 * Math.PI);
         context.fill();    
         context.closePath();
-    }
+
+
+        // let p1 = new Point(100, 40);
+        // let p2 = new Point(190, 68);
+
+        // var dx = p2.x - p1.x;
+        // var dy = p2.y - p1.y;
+        // var dist = Math.sqrt(dx*dx + dy*dy);
+    
+        // context.beginPath();
+
+        // context.arc(p1.x, p1.y, 10, 0, 2 * Math.PI);
+        // context.arc(p2.x, p2.y, 10, 0, 2 * Math.PI);
+        // context.fill();   
+        // context.closePath();
+        // // if (dist <= Math.abs(r2 - r1)) {
+        // //     // The circles are coinciding. There are no valid tangents.
+        // //     return;
+        // // }
+    
+        // // Rotation from the x-axis.
+        // var angle1 = Math.atan2(dy, dx);
+        // console.log(angle1);
+    
+        // // Relative angle of the normals. This is equal for both circles.
+        // var angle2 = Math.acos((10 - 10)/dist);
+        // console.log(angle2);
+    
+
+        // context.strokeStyle = 'blue';
+        // context.lineWidth = 1;
+        
+        // context.beginPath();
+        // context.moveTo(p1.x + 10 * Math.cos(angle1 + angle2), p1.y + 10 * Math.sin(angle1 + angle2));
+        // context.lineTo(p2.x + 10 * Math.cos(angle1 + angle2), p2.y + 10 * Math.sin(angle1 + angle2));
+        // context.stroke();                    
+        // context.closePath();
+
+        // context.beginPath();
+        // context.moveTo(p1.x + 10 * Math.cos(angle1 - angle2), p1.y + 10 * Math.sin(angle1 - angle2));
+        // context.lineTo(p2.x + 10 * Math.cos(angle1 - angle2), p2.y + 10 * Math.sin(angle1 - angle2));
+        // context.stroke();                    
+        // context.closePath();
+
+    }    
+
 }
